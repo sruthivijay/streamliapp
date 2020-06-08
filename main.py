897398b50589b931d103138d022f6f472aaba559
Sruthi Vijay <sruthiheidi@gmail.com>
2020-06-08 23:47:39 +0530

    added plots code

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from streamlit import caching
 import pandas as pd 
 import os
 import plotly.figure_factory as ff
+import plotly.express as px
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.graph_objects as gos
 
 st.title("Data Analysis Application")
 #st.write("enter file name")
@@ -22,6 +26,7 @@ activities = ["EDA","Plots","Model Building","About"]
 choice = st.sidebar.selectbox("Select Activities",activities)
 if choice == "EDA":
     enter_filename()
+    global data
     #caching.clear_cache()
     #uploaded_file = st.file_uploader("Choose an file...", type=["csv","xlsx"])
     if st.checkbox("upload file"):
@@ -58,18 +63,37 @@ if choice == "EDA":
     st.button("Distribution plots")
     if st.checkbox("Select columns"):
         columns = data.columns.tolist()
-        selected_columns = st.multiselect("Select variables",data.columns,default=None)
-        if selected_columns == []:
-            st.text("Please select a column")
-        else:
-            list_ = []
-            for column in selected_columns:
-                list_.append(data[column].values)
-            hist_data = list_
-            group_labels = selected_columns
-            colors = ['rgb(0, 0, 100)', 'rgb(0, 200, 200)']
-            fig1 = ff.create_distplot(hist_data, group_labels,colors)
-            fig1.update_layout(title_text='Customized Distplot')
-            st.plotly_chart(fig1)
-
+        feature = st.selectbox("Select feature",data.columns)
+        new_df2 = data[feature]
+        fig2 = px.histogram(new_df2, x=feature, marginal="rug")
+        st.plotly_chart(fig2)
+        # if selected_columns == []:
+        #     st.text("Please select a column")
+        # else:
+            
+            # list_ = []
+            # for column in selected_columns:
+            #     list_.append(data[column].values)
+            # hist_data = list_
+            # group_labels = selected_columns
+            # colors = ['rgb(0, 0, 100)', 'rgb(0, 200, 200)']
+            # fig1 = ff.create_distplot(hist_data, group_labels,colors)
+            # fig1.update_layout(title_text='Customized Distplot')
+            # st.plotly_chart(fig1)
+elif choice == "Plots":
+    filename = file_selector()
+    st.write('You selected `%s`' % filename)
+    data = pd.read_csv(filename)
+    species = st.multiselect('Show fearures', data.columns)
+    if species == []:
+        st.text("Please select a column")
+    elif len(species) == 1:
+        st.text("Please enter two columns")
+    else:
+        col1 = species[0]
+        col2 = species[1]
+        new_df = data[species]
+        st.write(new_df)
+        fig = px.scatter(new_df, x =col1,y=col2)
+        st.plotly_chart(fig)
 
